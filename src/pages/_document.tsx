fbb3eb40958d392aecf817391b7e6c4de33041c8
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-03-25 20:50:33 +0100

    tailwind settled up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta
+          name='description'
+          content='A powerful expense tracker app that allows you to upload CSV files from your bank, visualize your expenses and income, and gain better insight into your money flow.'
+        />
+        <meta
+          name='keywords'
+          content='Expense tracker, money tracker, expenses, CSV import, bank statement, financial management, personal finance, budgeting'
+        />
+        <meta name='author' content='Pablo Avilés' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+  darkMode: false, // or 'media' or 'class'
+};
