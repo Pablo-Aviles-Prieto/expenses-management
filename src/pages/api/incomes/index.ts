c6c6086fe992a71f5ebc12467a665094d8ba31ff
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-03-28 21:15:14 +0200

    WIP: API structure created

--- src/pages/api/incomes/index.ts
+++ src/pages/api/incomes/index.ts
@@ -5,8 +5,8 @@ type Data = {
   name: string
 }
 
-const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const endpointController = (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.status(200).json({ name: 'John Doe' })
 }
 
-export default handler
+export default endpointController
