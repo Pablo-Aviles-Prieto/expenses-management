ad25b234853e88fc4a538afbd658d430d75d2d30
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-04-16 01:52:15 +0200

    WIP: errors unificated

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,11 +1,13 @@
 export const URL_API = process.env.API_URL
 
 export const errorMessages = {
+  authorizedResource: 'Not Authorized for this resource',
   createUser: 'Error creating the user',
   credentials: 'Check the credentials provided',
   emailRegistered: 'Email already registered',
   generic: 'Something went wrong. Try again later',
   invalidUserId: 'Invalid user ID',
   methodAllowed: 'Method Not Allowed',
-  missingData: 'There is missing data in the request'
+  missingData: 'There is missing data in the request',
+  parsingImg: 'Unable to parse the image'
 }
