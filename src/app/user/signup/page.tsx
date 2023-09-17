4495bd0b0ec3e1955c29039063d625e8c0d0c33e
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-09-17 23:09:01 +0200

    Redirect signed in users from the signup path

--- src/app/user/signup/page.tsx
+++ src/app/user/signup/page.tsx
@@ -1,8 +1,21 @@
 import SignUp from '@/features/SignUp/SignUp'
+import { CustomSessionI } from '@/interfaces'
+import { authOptions } from '@/lib/authOptions'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
-// TODO: Check if the user is already signed in, dont load the SignUp component
-// using the getServerSession from next-auth
-const Page = () => {
+async function getSessionData() {
+  const session: CustomSessionI | null = await getServerSession(authOptions)
+  return session
+}
+
+const Page = async () => {
+  const session = await getSessionData()
+  console.log('session', session)
+
+  if (session && session.user) {
+    redirect(`/user/${session.user.id}/details`)
+  }
   return <SignUp />
 }
 
