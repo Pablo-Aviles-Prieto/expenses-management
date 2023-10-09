57b933e1373c6e19b2dab55ae3dfb387cc5fec7e
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-10-10 01:48:36 +0200

    Style bulk transactions table

--- src/features/Transactions/AddTransactions/components/BulkTransTable.tsx
+++ src/features/Transactions/AddTransactions/components/BulkTransTable.tsx
@@ -5,23 +5,39 @@ type Props = {
   bulkTransactions: TransactionBulk[]
 }
 
+const THEAD_CLASSES = 'bg-indigo-600 text-center px-1 py-2 border text-stone-100'
+const ROW_CLASSES = 'text-center text-base border p-1'
+
 export const BulkTransTable: FC<Props> = ({ bulkTransactions }) => {
   return (
-    <div className="w-full mt-4 max-h-[20rem] overflow-y-auto">
+    <>
       <h4 className="text-lg font-bold">Transaction list</h4>
-      <ul>
-        {bulkTransactions.map((transaction, i) => {
-          return (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={transaction.Concept + i.toString()}>
-              <span>Date: {transaction.Date}</span>
-              <span>Amount: {transaction.Amount}</span>
-              <span>Concept: {transaction.Concept}</span>
-              <span>Notes: {transaction.Notes}</span>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+      <div className="w-full mt-4 max-h-[22rem] overflow-y-auto">
+        <table className="min-w-full border-collapse border-gray-300">
+          <thead className="sticky -top-[1px]">
+            <tr>
+              <th className={`w-[18%] ${THEAD_CLASSES}`}>Date</th>
+              <th className={`w-[10%] ${THEAD_CLASSES}`}>Amount</th>
+              <th className={`w-[40%] ${THEAD_CLASSES}`}>Name (Concept)</th>
+              <th className={`w-[15%] ${THEAD_CLASSES}`}>Notes</th>
+              <th className={`w-[15%] ${THEAD_CLASSES}`}>Categories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bulkTransactions.map((transaction, i) => {
+              return (
+                <tr key={transaction.Concept + transaction.Date + i.toString()}>
+                  <td className={`${ROW_CLASSES}`}>{transaction.Date}</td>
+                  <td className={`${ROW_CLASSES}`}>{transaction.Amount}</td>
+                  <td className={`text-sm !text-left ${ROW_CLASSES}`}>{transaction.Concept}</td>
+                  <td className={`text-sm !text-left ${ROW_CLASSES}`}>{transaction.Notes}</td>
+                  <td className={`${ROW_CLASSES}`}>Add cat</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
